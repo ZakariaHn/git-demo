eed0ded135c138cf0cc5dd6fd885d9f6c0f6f139
Satyajit <jadeja.satyajit.m@gmail.com>
2023-04-03 14:14:29 +0200

    changed h2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Blah</h1>
+        <h2>Content Changed</h2>
       </header>
     </div>
   );
